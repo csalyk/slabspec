f4b575ebf649752a83abdaf9117d76a9444be4c8
Klaus Pontoppidan <pontoppi@stsci.edu>
2023-02-28 20:11:04 -0500

    updated setup to be called slabspec, consistent with the repo

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="flux_calculator",
-    version="0.0.1",
+    name="slabspec",
+    version="1.0",
     author="Colette Salyk",
     author_email="cosalyk@vassar.edu",
-    description="A package to perform line fits for IR spectroscopic data.",
+    description="A package to calculate molecular slab models for IR spectroscopic data.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
@@ -17,4 +17,4 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-)    
+)

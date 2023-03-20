Fixed line flux units to output in W/m2

slabspec/slabspec.py
@@ -254,7 +254,6 @@ def make_spec(molecule_name, n_col, temp, area, wmax=40, wmin=1, deltav=None, is
     efactor2 = hitran_data['eup_k']/temp
     efactor1 = hitran_data['elower']*1.e2*h.value*c.value/k_B.value/temp
     tau0 = afactor*(np.exp(-1.*efactor1)-np.exp(-1.*efactor2))*phia  #Avoids numerical issues at low T
-    w0 = 1.e6/wn0
 
     dvel = deltav/oversamp    #m/s
     nvel = 10*oversamp+1 #5 sigma window
@@ -296,14 +295,13 @@ def make_spec(molecule_name, n_col, temp, area, wmax=40, wmin=1, deltav=None, is
         if(w.size > 0):
             newtau = np.interp(totalwave[w],wave[i,:], tau[i,:])
             totaltau[w] += newtau
-            f_arr[i,:] = 2*h.value*c.value*wn0[i]**3./(np.exp(wnfactor[i])-1.0e0)*(1-np.exp(-tau[i,:]))*si2jy*omega
-            lineflux_jykms = np.sum(f_arr[i,:])*dvel
-            lineflux[i] = lineflux_jykms*1e-26*1.*1e5*(1./(w0[i]*1e-4)) #mks
+            f_arr[i,:] = 2*h.value*c.value*wn0[i]**3./(np.exp(wnfactor[i])-1.0e0)*(1-np.exp(-tau[i,:]))*omega
+            lineflux[i] = np.sum(f_arr[i,:]) * (dvel/c.value) * (c.value*wn0[i]) #in W/m2
 
     wave_arr = wave
     wn = 1.e6/totalwave #m^{-1}
     wnfactor = h.value*c.value*wn/(k_B.value*temp)
-    flux = 2*h.value*c.value*wn**3./(np.exp(wnfactor)-1.0e0)*(1-np.exp(-totaltau))*si2jy*omega
+    flux = 2*h.value*c.value*wn**3./(np.exp(wnfactor)-1.0e0)*(1-np.exp(-totaltau))*si2jy*omega #in Jy
 
     wave = totalwave
 